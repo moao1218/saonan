f19017793ba1b761580f9b65acc29c3a01f2dfc5
lmx2019 <l.m.x.2009@163.com>
2019-05-13 19:21:42 +0800

    小改

--- src/main/java/cn/saonan/control/ClerkController.java
+++ src/main/java/cn/saonan/control/ClerkController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -20,27 +21,27 @@ public class ClerkController {
 
 	@Autowired
 	private ClerkService clerkservice;
-	
-	@ResponseBody
-	@GetMapping(value="/findClerkSplit")
 
+	@RequestMapping(value = "/findClerkSplit")
 	public String findClerkSplit(Model model,Clerk clerk) {
-		Map<String,Object> map = new HashMap<String,Object>();
-		int cp = 1;
-		int ps = 5;
-		map.put("cp", cp);
-		map.put("ps", ps);
-		clerkservice.findClerkSplits(map);
-		List<Clerk> list = new ArrayList<Clerk>();
-		List<Clerk> clerklist = (List<Clerk>) map.get("clerks");
-		int count = (Integer)map.get("c_count");
-		int totalpage=(Integer)map.get("totalpage");
-		model.addAttribute("cp", cp);
-		model.addAttribute("ps", ps);
-		model.addAttribute("totalpage", totalpage);
-		model.addAttribute("clerklist", clerklist);
 		
+		  Map<String,Object> map = new HashMap<String,Object>(); 
+		  int cp = 1; 
+		  int ps =5;
+		  map.put("cp", cp); 
+		  map.put("ps", ps); 
+		  clerkservice.findClerkSplits(map);
+		  
+		  List<Clerk> clerklist =(List<Clerk>) map.get("clerks"); 
+		  int count = (Integer)map.get("c_count"); 
+		  int totalpage = (Integer) map.get("totalpage"); 
+		  model.addAttribute("cp", cp);
+		  model.addAttribute("ps", ps); 
+		  model.addAttribute("count",count);
+		  model.addAttribute("totalpage", totalpage);
+		  model.addAttribute("clerklist", clerklist);
+		 
 		return "server/findAllClerk";
-		
+
 	}
 }
