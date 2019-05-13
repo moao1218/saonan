60a88af2d37272fcd802bc669f24ed8d68c17e8e
JAVA-XFM <3030332259@qq.com>
2019-05-13 14:28:44 +0800

    删除了原来的所有属性和方法,按照数据库表重新创建了users表

--- src/main/java/cn/saonan/pojo/Users.java
+++ src/main/java/cn/saonan/pojo/Users.java
@@ -2,151 +2,84 @@ package cn.saonan.pojo;
 
 import java.io.Serializable;
 
+
+/**
+ *  客户实体类
+ * @author ThinkPad
+ *
+ */
 public class Users implements Serializable {
 
-	private Integer magid;
-	private String	username;
-	private String	userpwd;
-	private String	job;
-	private String	realname;
-	private String	email;
-	private String	sex;
-	private String	nation;
-	private String	address;
-	private String	linkman;
-	private String	emergency;
-	private String	join_date;
-	private String	area;
-	private String	phone;
-	private String	del_status;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1908612469334711910L;
 	
-	private	Integer roleid;
-
-	public Integer getMagid() {
-		return magid;
+	
+	private String userid; 
+	private String username;
+	private String userpwd;
+	private String realname; 
+	private String phone;
+	private String join_date; 
+	private String bank_card; 
+	private String del_status;
+	private String yuliu;
+	public String getUserid() {
+		return userid;
 	}
-
-	public void setMagid(Integer magid) {
-		this.magid = magid;
+	public void setUserid(String userid) {
+		this.userid = userid;
 	}
-
 	public String getUsername() {
 		return username;
 	}
-
 	public void setUsername(String username) {
 		this.username = username;
 	}
-
 	public String getUserpwd() {
 		return userpwd;
 	}
-
 	public void setUserpwd(String userpwd) {
 		this.userpwd = userpwd;
 	}
-
-	public String getJob() {
-		return job;
-	}
-
-	public void setJob(String job) {
-		this.job = job;
-	}
-
 	public String getRealname() {
 		return realname;
 	}
-
 	public void setRealname(String realname) {
 		this.realname = realname;
 	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getSex() {
-		return sex;
-	}
-
-	public void setSex(String sex) {
-		this.sex = sex;
-	}
-
-	public String getNation() {
-		return nation;
-	}
-
-	public void setNation(String nation) {
-		this.nation = nation;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getLinkman() {
-		return linkman;
-	}
-
-	public void setLinkman(String linkman) {
-		this.linkman = linkman;
-	}
-
-	public String getEmergency() {
-		return emergency;
-	}
-
-	public void setEmergency(String emergency) {
-		this.emergency = emergency;
-	}
-
-	public String getJoin_date() {
-		return join_date;
-	}
-
-	public void setJoin_date(String join_date) {
-		this.join_date = join_date;
-	}
-
-	public String getArea() {
-		return area;
-	}
-
-	public void setArea(String area) {
-		this.area = area;
-	}
-
 	public String getPhone() {
 		return phone;
 	}
-
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-
+	public String getJoin_date() {
+		return join_date;
+	}
+	public void setJoin_date(String join_date) {
+		this.join_date = join_date;
+	}
+	public String getBank_card() {
+		return bank_card;
+	}
+	public void setBank_card(String bank_card) {
+		this.bank_card = bank_card;
+	}
 	public String getDel_status() {
 		return del_status;
 	}
-
 	public void setDel_status(String del_status) {
 		this.del_status = del_status;
 	}
-
-	public Integer getRoleid() {
-		return roleid;
+	public String getYuliu() {
+		return yuliu;
 	}
-
-	public void setRoleid(Integer roleid) {
-		this.roleid = roleid;
+	public void setYuliu(String yuliu) {
+		this.yuliu = yuliu;
 	}
+	
+	
+	
 }
