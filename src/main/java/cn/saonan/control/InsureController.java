9932bd31e91347309b86f22b2f30f37ef8c37b7f
moao1218 <282697282@qq.com>
2019-05-13 16:01:26 +0800

    投保单列表分页完成

--- src/main/java/cn/saonan/control/InsureController.java
+++ src/main/java/cn/saonan/control/InsureController.java
@@ -7,25 +7,45 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import cn.saonan.pojo.City;
+import cn.saonan.pojo.Clerk;
 import cn.saonan.pojo.InsuranceSlip;
 import cn.saonan.service.InsuranceSlipService;
+import cn.saonan.service.UsersService;
 
 @Controller
 public class InsureController {
 	
 	@Autowired
 	private InsuranceSlipService insuranceSlipService ;
+	
+	@Autowired
+	private UsersService usersService;
 
-	@GetMapping(value="/jumpInsuranceList")
-	public String goList(Model model) throws ParseException {
+	@RequestMapping(value="/jumpInsuranceList")
+	public String goList(Model model,HttpServletRequest request) throws ParseException {
 		Map<String,Object> map = new HashMap<String,Object>();
 		int cp = 1;
 		int ps = 5;
+		
+		String cpp = request.getParameter("cp");
+		
+		if(cpp!=null&&!"".equals(cpp)){
+			cp = Integer.parseInt(cpp);
+		}
+		
+		String pss = request.getParameter("ps");
+		if(pss!=null&&!"".equals(pss)){
+			ps = Integer.parseInt(pss);
+		}
+		
 		map.put("cp", cp);
 		map.put("ps", ps);
 		insuranceSlipService.findInsuranceSlipList(map);
@@ -44,9 +64,74 @@ public class InsureController {
 			}
 //			System.out.println(date.format(new Date(parse.getTime()+12*60*60*1000)));
 		}
-		Integer count = (Integer) map.get("v_count");
+		Integer v_count = (Integer) map.get("v_count");
+		int totalpage = (v_count-1)/ps + 1;
+		model.addAttribute("cp", cp);
+		model.addAttribute("ps", ps);
+		
+		List<City> cityList = usersService.findAllCity();
 		model.addAttribute("insureList", insureList);
+		model.addAttribute("totalpage", totalpage);
+		model.addAttribute("cityList", cityList);
 		return "server/insurance_slip_list";
 	}
 	
+	
+	@RequestMapping(value="/jumpPending")
+	public String pending(Model model,HttpServletRequest request) throws ParseException {
+		Map<String,Object> map = new HashMap<String,Object>();
+		int cp = 1;
+		int ps = 5;
+		
+		Clerk clerk = (Clerk) request.getSession().getAttribute("user");
+		Integer roleid = clerk.getRoleid();
+		String v_role = "";
+		if(roleid==1) {
+			v_role = "1";
+		}else if(roleid==2) {
+			v_role = "7";
+		}else if(roleid==3) {
+			v_role = "11";
+		}
+		
+		String cpp = request.getParameter("cp");
+		
+		if(cpp!=null&&!"".equals(cpp)){
+			cp = Integer.parseInt(cpp);
+		}
+		
+		String pss = request.getParameter("ps");
+		if(pss!=null&&!"".equals(pss)){
+			ps = Integer.parseInt(pss);
+		}
+		
+		map.put("cp", cp);
+		map.put("ps", ps);
+		insuranceSlipService.findInsuranceSlipList(map);
+		List<InsuranceSlip> insureList = (List<InsuranceSlip>) map.get("insureList");
+		
+		Date d = new Date();
+		SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd");
+		for (InsuranceSlip insuranceSlip : insureList) {
+			Date parse = date.parse(insuranceSlip.getInsure_date());
+			if(d.getTime()<parse.getTime()+12*60*60*1000) {
+				insuranceSlip.setUrg("正常");
+			}else if(d.getTime()<parse.getTime()+24*60*60*1000) {
+				insuranceSlip.setUrg("预警");
+			}else {
+				insuranceSlip.setUrg("报警");
+			}
+//			System.out.println(date.format(new Date(parse.getTime()+12*60*60*1000)));
+		}
+		Integer v_count = (Integer) map.get("v_count");
+		int totalpage = (v_count-1)/ps + 1;
+		model.addAttribute("cp", cp);
+		model.addAttribute("ps", ps);
+		
+		List<City> cityList = usersService.findAllCity();
+		model.addAttribute("insureList", insureList);
+		model.addAttribute("totalpage", totalpage);
+		model.addAttribute("cityList", cityList);
+		return "server/insurance_slip_list";
+	}
 }
--- src/main/java/cn/saonan/mapper/UsersMapper.java
+++ src/main/java/cn/saonan/mapper/UsersMapper.java
@@ -1,8 +1,22 @@
 package cn.saonan.mapper;
 
+import java.util.List;
+
+import cn.saonan.pojo.City;
 import cn.saonan.pojo.Clerk;
 
 public interface UsersMapper {
 
+	
+	/**职员登录验证
+	 * @param user
+	 * @return
+	 */
 	public Clerk isLogin(Clerk user);
+	
+	
+	/**查找广东省下的所有城市
+	 * @return
+	 */
+	public List<City> findAllCity();
 }
