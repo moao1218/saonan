82875a92986c4bdc9090b373d9967f1a1d2c2aaf
JAVA-XFM <3030332259@qq.com>
2019-05-14 09:22:17 +0800

    添加了身份验证工具类,创建了物品清单实体列,创建了物品勘察实体列

---
+++ src/main/java/cn/saonan/utils/IdCard.java
@@ -0,0 +1,48 @@
+package cn.saonan.utils;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class IdCard {
+	
+	/**
+	 * 	检查身份证是否合理
+	 * @param id
+	 * @return
+	 */
+	public static String checkIdCard(String id) {
+		
+		StringBuffer b=new StringBuffer();
+		try {
+
+			URL u=new URL("https://way.jd.com/yingyan/queryidcard?id=" + id + "&appkey=a70c18a3572454a1eadd63553edab236");
+			URLConnection openConnection = u.openConnection();
+			InputStream inputStream = openConnection.getInputStream();
+			InputStreamReader is = new InputStreamReader(inputStream);
+			BufferedReader bis = new BufferedReader(is);
+			String readLine = bis.readLine();
+		for(;readLine!=null;) {
+			//System.out.println(readLine);
+			b.append(readLine);
+			readLine = bis.readLine();
+		}
+		
+		System.out.println(b.toString());
+		/*
+		 * JSON parse = (JSON) JSON.parse(b.toString());
+		 * System.out.println(parse.toString());
+		 */
+		
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+		
+		return "b";
+		
+	}
+
+}
