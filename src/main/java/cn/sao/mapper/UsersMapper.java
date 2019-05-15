3eb331fc61abbd81c4049550bb08797cd2ea89d2
1689151249 <1689151249@qq.com>
2019-05-15 09:20:56 +0800

    # WARNING: head commit changed in the meantime
    hyz

--- src/main/java/cn/sao/mapper/UsersMapper.java
+++ src/main/java/cn/sao/mapper/UsersMapper.java
@@ -4,4 +4,17 @@ import cn.sao.pojo.Users;
 
 public interface UsersMapper {
 
+	/**
+	 * 用户账号密码登录
+	 * @param users
+	 * @return
+	 */
+	public Users isLogin(Users users);
+	
+	/**
+	 * 用户短信验证码登录
+	 * @param users
+	 * @return
+	 */
+	public Users Login(Users users);
 }
