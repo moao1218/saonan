ea425ed1c5ce8959ec795b161a0aa88a2234d1ae
1689151249 <1689151249@qq.com>
2019-05-16 11:39:07 +0800

    hyz

--- src/main/java/cn/saonan/mapper/CoverageMapper.java
+++
@@ -1,5 +0,0 @@
-package cn.saonan.mapper;
-
-public interface CoverageMapper {
-
-}
--- src/main/java/cn/saonan/service/RolessService.java
+++
@@ -1,9 +0,0 @@
-package cn.saonan.service;
-
-import cn.saonan.pojo.Roless;
-
-public interface RolessService {
-	
-	public String findjob(Integer roleid);
-	
-}
