419a47b0cbcd86a8b778adb11d7f86a7cea8802c
lmx2019 <l.m.x.2009@163.com>
2019-05-16 15:09:08 +0800

    lmx

--- src/main/java/cn/saonan/utils/BCryptInterface.java
+++ src/main/java/cn/saonan/utils/BCryptInterface.java
@@ -1,18 +1,18 @@
 package cn.saonan.utils;
 
 public interface BCryptInterface {
-	/**��ȡ����
+	/**获取密文
 	 * 
-	 * @param ԭ����
-	 * @param ��
+	 * @param 原密码
+	 * @param 盐
 	 * @return
 	 */
 	public String getCode(String original,Integer salt);
 	
 	/**
 	 * 
-	 * @param ��������
-	 * @param ��������
+	 * @param 测试密码
+	 * @param 密码密文
 	 * @return
 	 */
 	public boolean checkMatch(String testCode,String Code);
--- src/main/java/cn/saonan/utils/ClientSocketInterface.java
+++ src/main/java/cn/saonan/utils/ClientSocketInterface.java
@@ -3,8 +3,8 @@ package cn.saonan.utils;
 public interface ClientSocketInterface {
 	/**
 	 * 
-	 * @param ����json
-	 * @return ���ص����ݰ�
+	 * @param 命令json
+	 * @return 返回的数据包
 	 */
 	public String sendAndGet(String cmd);
 }
